bbaa7e6b4b69aa0b4c1976f2cdf7a6e846e66a00
asamalik <asamalik@redhat.com>
2017-04-27 15:45:22 +0200

    cleanup

--- args.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Dependency visualizer.')
-parser.add_argument('--build', action="store_true", help='resolve build instead of runtime dependencies')
-parser.add_argument('-l', '--max-level', type=int, default=1, help='maximum level of recursive dependencies')
-parser.add_argument('packages', metavar='PACKAGE', type=str, nargs='+', help='packages to be analyzed')
-args = parser.parse_args()
-
-print args
