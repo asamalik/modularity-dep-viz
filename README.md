2d6475d920c34e28f4cc03d09b5eb4c1b48269cc
asamalik <asamalik@redhat.com>
2017-04-27 14:30:53 +0200

    some fixes

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ RPM dependency visualisation in Modularity
 This script visualizes build and runtime dependnecies of RPM packages. It's a pretty early version developed iteratively (read: hacked, ugly, steaming pile of anti-pattern garbage).
 
 Based on [Harald's script](https://harald.hoyer.xyz/2014/01/14/self-hosting-fedora-base/).
+
+## Usage
+
+1. Edit the script to choose between runtime/build deps and number of levels for recursive resolving
+2. Generate the deps `python dep-viz.py nginx > out.dot`
+3. Make it nice `START=7; cat out.dot | sfdp -Gstart=$START -Goverlap=prism | gvmap -e -d $START | neato -Gstart=$START -n -Ecolor=#44444455 -Tsvg > out.svg`
--- dep-viz.py
+++ dep-viz.py
@@ -9,9 +9,18 @@ import subprocess
 # Build or runtime deps? True is build, False runtime
 option_build_requires = True
 
-# Number of levels of requires
-option_maximum_of_levels = 2
+# Runtime deps output binary RPMs. Set to True to output source SRPMSs.
+# The script will run much longer.
+option_just_sources = False
 
+# Number of levels of requires
+option_maximum_of_levels = 3
+
+# END of settings
+
+# build deps are srpms anyway, so let's not waste time...
+if option_build_requires:
+    option_just_sources = False
 
 
 def getBuildRequires(pkgset):
@@ -56,6 +65,14 @@ def getSRPMs(requires):
         ret.add(prov[:t])
     return ret
 
+def getSRPM(package):
+    cmd = ["repoquery", "--disablerepo=*", "--enablerepo=fedora", "--enablerepo=fedora-source", "--whatprovides", "--source", package]
+    out = subprocess.check_output(cmd)
+
+    prov =  out.split("\n")[0]
+    name = "-".join(prov.split("-")[:-2])
+    return name
+
 def getBaseRuntimePkgs():
     ret = set()
     f = open('base_runtime_packages.txt', 'r')
@@ -70,6 +87,9 @@ oldpkgset = set()
 newpkgset = set(getSRPMs(sys.argv[1:]))
 
 ignoreset = set([
+    "generic-release",
+    "fedora-release",
+    "fedora-repos",
 ])
 
 level = 0
@@ -78,6 +98,15 @@ print "node [fontname=monospace];"
 
 # Highlighted modules
 modules = set()
+
+# highlighted packages - just to make sure they are not highlighted twice
+# which was happening when 1st lvl dep was also a 3rd lvl dep, so the pkg got highlighted as 3rd lvl
+highlighted_pkgs = set()
+
+# highlight input distinctively
+for pkg in newpkgset:
+    print '\t"%s" [color=red,fontcolor=green,fontsize=40];' % pkg
+    highlighted_pkgs.add(pkg)
 
 while (len(newpkgset - oldpkgset) > 0) and (level < option_maximum_of_levels):
     diff = newpkgset - oldpkgset
@@ -93,55 +122,84 @@ while (len(newpkgset - oldpkgset) > 0) and (level < option_maximum_of_levels):
 
         # i is package
 
+        # show just source packages?
+        if option_just_sources:
+            i_print = getSRPM(i)
+        else:
+            i_print = i
+
         # Visualise every dependency differently.
-        if level is 0:
-            print '\t"%s" [color=red,fontcolor=green,fontsize=40];' % i
-        elif level is 1:
-            print '\t"%s" [fontsize=25];' % i
-        elif level is 2:
-            print '\t"%s" [fontcolor="#003380",fontsize=20];' % i
-        elif level is 3:
-            print '\t"%s" [fontcolor="#0055d4",fontsize=16];' % i
-        elif level is 4:
-            print '\t"%s" [fontcolor="#2a7fff",fontsize=14];' % i
+        #if i_print not in highlighted_pkgs:
+        #    highlighted_pkgs.add(i_print)
+        #    if level is 0:
+        #        print '\t"%s" [color=red,fontcolor=green,fontsize=40];' % i_print
+        #    elif level is 1:
+        #        print '\t"%s" [fontsize=25];' % i_print
+        #    elif level is 2:
+        #        print '\t"%s" [fontcolor="#003380",fontsize=20];' % i_print
+        #    elif level is 3:
+        #        print '\t"%s" [fontcolor="#0055d4",fontsize=16];' % i_print
+        #    elif level is 4:
+        #        print '\t"%s" [fontcolor="#2a7fff",fontsize=14];' % i_print
 
         if len(req):
             for j in req:
 
                 # j is a require of the package
 
+                # show just source packages?
+                if option_just_sources:
+                    j_print = getSRPM(j)
+                else:
+                    j_print = j
+
+
+
                 # Base Runtime module
                 if j in brt_pkgs:
+                    j_print = "BASE-RUNTIME"
                     if "BASE-RUNTIME" not in modules:
                         modules.add("BASE-RUNTIME")
                         print '\t"BASE-RUNTIME" [fontcolor="#666666",fontsize=30];'
-                    print '\t"%s" -> "%s";' % (i,"BASE-RUNTIME")
                     newpkgset.discard(j)
 
                 # Perl module
                 elif j == "perl" or (j[:5] == "perl-"):
+                    j_print = "PERL"
                     if "PERL" not in modules:
                         modules.add("PERL")
                         print '\t"PERL" [fontcolor="#666666",fontsize=30];'
-                    print '\t"%s" -> "%s";' % (i,"PERL")
                     newpkgset.discard(j)
 
                 # Python module
-                elif j == "python" or j == "python2" or j == "python3" or (j[:7] == "python-") or (j[:8] == "python2-") or (j[:8] == "python3-"):
-                    if "PYTHON" not in modules:
-                        modules.add("PYTHON")
-                        print '\t"PYTHON" [fontcolor="#666666",fontsize=30];'
-                    print '\t"%s" -> "%s";' % (i,"PYTHON")
-                    newpkgset.discard(j)
+                #elif j == "python" or j == "python2" or j == "python3" or (j[:7] == "python-") or (j[:8] == "python2-") or (j[:8] == "python3-"):
+                #    j_print = "PYTHON"
+                #    if "PYTHON" not in modules:
+                #        modules.add("PYTHON")
+                #        print '\t"PYTHON" [fontcolor="#666666",fontsize=30];'
+                #    newpkgset.discard(j)
 
                 # ignored packages
                 elif j in ignoreset:
-                    print '\t"%s" [fontcolor=red];' % j
-                    print '\t"%s" -> "%s";' % (i,j)
+                    print '\t"%s" [fontcolor=red];' % j_print
+                    print '\t"%s" -> "%s";' % (i_print, j_print)
                     newpkgset.discard(j)
 
-                else:
-                    print '\t"%s" -> "%s";' % (i,j)
+
+                # Visualise every dependency differently.
+                if j_print not in highlighted_pkgs and j_print not in modules:
+                    highlighted_pkgs.add(j_print)
+                    if level is 0:
+                        print '\t"%s" [fontsize=25];' % j_print
+                    elif level is 1:
+                        print '\t"%s" [fontcolor="#003380",fontsize=20];' % j_print
+                    elif level is 2:
+                        print '\t"%s" [fontcolor="#0055d4",fontsize=16];' % j_print
+                    elif level is 3:
+                        print '\t"%s" [fontcolor="#2a7fff",fontsize=14];' % j_print
+
+                # visualize relation
+                print '\t"%s" -> "%s";' % (i_print, j_print)
 
     level+=1
 print "}"
